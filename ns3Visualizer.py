38851dedb232a982058db7d1b6d4f0ea84641b37
xlejko <xlejko@stuba.sk>
2022-04-16 13:32:07 +0200

    zoom and scroll

--- ns3Visualizer.py
+++ ns3Visualizer.py
@@ -1,100 +1,169 @@
 import tkinter as tk
+from tkinter import font
 import xml.dom.minidom
 from types import SimpleNamespace
-from queue import Queue
 from enum import Enum
 from threading import Thread
+import queue
+import multiprocessing
+
+pressed = False
+fontSize = 10
 
 
 class Messages(Enum):
-    CLICK = 0
+    CLICK = 0,
+    STOP = 0,
 
 
 class Node:
     def __init__(self, canvas, color, posx, posy):
-        print("test init")
         self.canvas = canvas
         self.color = color
         self.posx = posx
         self.posy = posy
-        #canvas.create_oval(10, 10, 40, 40, outline="black", fill=self.color, width=1)
-        canvas.create_oval(self.posx-15, self.posy-15, self.posx+15, self.posy+15, outline="black", fill=self.color, width=1)
+        canvas.create_oval(self.posx + 85, self.posy+85, self.posx+115, self.posy+115, outline="black", fill=self.color, width=1)
 
-
-    def moveNode(self, posx, posy):
-        print("move node")
-        print(posx, posy)
-        self.canvas.move(self, posx, posy)
-
-
+storeNodes = list()
 def readXML(myCanvas):
     doc = xml.dom.minidom.parse("ns3.xml")
     nodes = doc.getElementsByTagName("node")
     nu = doc.getElementsByTagName("nu")
     print("%d nodes: " % nodes.length)
     count = 0
-    storeNodes = list()
+    
     for node in nodes:
-        posX = int(node.getAttribute("locX"))
-        posY = int(node.getAttribute("locY"))
-
-        rval = int(nu[count].getAttribute("r"))
-        gval = int(nu[count].getAttribute("g"))
-        bval = int(nu[count].getAttribute("b"))
-        color = '#%02x%02x%02x' % (rval, gval, bval)
-        print(color)
-
-        #circlenode = myCanvas.create_oval(10, 10, 40, 40, outline="black", fill="green", width=1)
-        circle = Node(myCanvas, color, posX, posY)
+        color = '#%02x%02x%02x' % (int(nu[count].getAttribute("r")), int(nu[count].getAttribute("g")), int(nu[count].getAttribute("b")))
+        circle = Node(myCanvas, color, int(node.getAttribute("locX")), int(node.getAttribute("locY")))
         storeNodes.append(circle)
-        #circle.moveNode(posX, posY)
-        #myCanvas.move(circlenode, posX, posY)
         count = count + 1
 
 
 def updateCycle(guiRef, queue):
+    print(queue)
     while True:
         msg = queue.get()
+    
+        print(msg)
         if msg == Messages.CLICK:
             mycanvas = guiRef.canvas
             readXML(mycanvas)
+        if msg == Messages.STOP:
+            mycanvas = guiRef.canvas
+            mycanvas.bind("<ButtonPress-1>", lambda event:move_start(event, mycanvas))
+            mycanvas.bind("<B1-Motion>", lambda event:move_move(event, mycanvas))
+
+            mycanvas.bind("<ButtonPress-2>", lambda event:pressed2(event, mycanvas))
+            mycanvas.bind("<Motion>",  lambda event:move_move2(event, mycanvas))
+
+            #linux scroll
+            mycanvas.bind("<Button-4>", lambda event:zoomerP(event, mycanvas))
+            mycanvas.bind("<Button-5>",lambda event:zoomerM(event, mycanvas))
+            # windows scroll
+            mycanvas.bind("<MouseWheel>",lambda event:zoomer(event, mycanvas))
+
+                    
 
 
 def create_grid(canvas):
-    w = canvas.winfo_reqwidth()-6 # Get current width of canvas
-    h = canvas.winfo_reqheight()-6 # Get current height of canvas
+    w = canvas.winfo_reqwidth()-4 # Get current width of canvas
+    h = canvas.winfo_reqheight()-4 # Get current height of canvas
     print(w,h)
     canvas.delete('grid_line') # Will only remove the grid_line
 
     # Creates all vertical lines at intevals of 100
-    for i in range(0, w, 100):
-        canvas.create_line([(i, 0), (i, h)], tag='grid_line')
+    for i in range(0, w*2, 100):
+        canvas.create_line([(i, 0), (i, h*2)], tag='grid_line')
 
     # Creates all horizontal lines at intevals of 100
-    for i in range(0, h, 100):
-        canvas.create_line([(0, i), (w, i)], tag='grid_line')
+    for i in range(0, h*2, 100):
+        canvas.create_line([(0, i), (w*2, i)], tag='grid_line')
+
+    canvas.create_line([(0, 1000), (w*2, 1000)], tag='grid_line')
+    canvas.create_line([(1000, 0), (1000, h*2)], tag='grid_line')
+
+#move
+def move_start(event, canvas):
+    canvas.scan_mark(event.x, event.y)
+
+def move_move(event, canvas):
+    canvas.scan_dragto(event.x, event.y, gain=1)
+
+#move
+def pressed2(event, canvas):
+    global pressed
+    pressed = not pressed
+    canvas.scan_mark(event.x, event.y)
+
+def move_move2(event, canvas):
+    if pressed:
+        canvas.scan_dragto(event.x, event.y, gain=1)
+
+        #windows zoom
+def zoomer(event, canvas):
+    global fontSize
+    if (event.delta > 0):
+        canvas.scale("all", event.x, event.y, 1.1, 1.1)
+        fontSize = fontSize * 1.1
+    elif (event.delta < 0):
+        canvas.scale("all", event.x, event.y, 0.9, 0.9)
+        fontSize = fontSize * 0.9
+    canvas.configure(scrollregion = canvas.bbox("all"))
+    for child_widget in canvas.find_withtag("text"):
+        canvas.itemconfigure(child_widget, font=("Helvetica", int(fontSize)))
+
+#linux zoom
+def zoomerP(event, canvas):
+    canvas.scale("all", event.x, event.y, 1.1, 1.1)
+    canvas.configure(scrollregion = canvas.bbox("all"))
+def zoomerM(event, canvas):
+    canvas.scale("all", event.x, event.y, 0.9, 0.9)
+    canvas.configure(scrollregion = canvas.bbox("all"))
 
 
 
-def gui(root, queue):
+def gui(frame, queue):
     label = tk.StringVar()
     label.set("Control Panel")
-    tk.Label(root, textvariable=label).pack()
-    startbtn = tk.Button(root, text="SIMULATE", command=lambda: queue.put(Messages.CLICK))
-    startbtn.pack(ipadx=20, ipady=10)
-    canvas = tk.Canvas(root, width=500, height=500, highlightbackground="black")
-    canvas.pack(fill=tk.BOTH, expand=True, padx=10, pady=10, ipadx=100, ipady=100)
+    tk.Label(frame, textvariable=label).grid(row=1, column=0)
+    startbtn = tk.Button(frame, text="SIMULATE", command=lambda: queue.put(Messages.CLICK))
+    startbtn.grid(row=2, column=0)
+    nodeBtn = tk.Button(frame, text="STOP", command=lambda: queue.put(Messages.STOP))
+    nodeBtn.grid(row=2, column=3)
+
+    canvas = tk.Canvas(frame, width=500, height=500, highlightbackground="black")
+    xsb = tk.Scrollbar(frame, orient="horizontal", command=canvas.xview)
+    ysb = tk.Scrollbar(frame, orient="vertical", command=canvas.yview)
+    canvas.configure(yscrollcommand=ysb.set, xscrollcommand=xsb.set)
+    canvas.configure(scrollregion=(0,0,1000,1000))
+
+    xsb.grid(row=4, column=0, sticky="ew")
+    ysb.grid(row=3, column=1, sticky="ns")
+    canvas.grid(row=3, column=0, sticky="nsew", ipadx=100, ipady=100)
+    global fontSize
+    frame.fontSize = fontSize
+
+
+
+
+    
+
+
     create_grid(canvas)
-    return SimpleNamespace(label=label, canvas=canvas)
+    return SimpleNamespace(canvas=canvas)
+
+
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    root.geometry("500x500")
+    root.geometry("800x800")
     root.title("Visualiser")
-    queue = Queue()
-    guiRef = gui(root, queue)
+    queue = queue.Queue() 
+    frame = tk.Frame(root, bg="red")
+    frame.pack(fill="both", expand=True)
+    guiRef = gui(frame, queue)
     t = Thread(target=updateCycle, args=(guiRef, queue,))
     t.daemon = True
     t.start()
-    tk.mainloop()
+    root.mainloop()
